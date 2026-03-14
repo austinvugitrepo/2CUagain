10fe3bb3d7723b625553619b3f74db1f8487b70d
Austin Vu <freeluck.v.i.p999@gmail.com>
2026-03-14 02:42:41 -0500

    start of binary search project

--- binary.c
+++ binary.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 
 int main(){
-
-
-printf("Hello World!\n");
-
-
-
+	
+int size = 0;
+printf("What is the size of the array?: ");
+scanf("%d", &size);
+// "%d" represents integer
+printf("The array can hold %d elements\n", size);
+//type casting so I can maintain portability even with C++ compiler
+int* arr = (int*)malloc(size * sizeof(int));
+printf("Enter the elements in the array: ");
+for(int i = 0; i < size; i++){
+scanf("%d", &arr[i]);  
+}
+printf("Your array: ");
+for(int i = 0; i < size; i++){
+printf("%d", arr[i]);
+}
+printf("\n");
+free(arr);
 return 0;
 }
