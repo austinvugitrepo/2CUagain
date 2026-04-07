d7d5313b33ee9505400d5c69576ab1240946d8ec
Austin <freeluck.v.i.p999@gmail.com>
2026-04-07 17:04:10 -0500

    tried my first OpenBSD KNF style program

---
+++ hworldknf.c
@@ -0,0 +1,20 @@
+/* This is my first time trying to write a C program in OpenBSD's KNF style. 
+ * I am going to try writing a hello world program with the restrictions.
+ * I am going to be using the man page for style as my reference.
+ * I will also being using the src code as a guide as well.
+ * My biggest hurdle is going to be this 80 column thing for sure.
+ * It is oddly satisfying writing code with these restrictions though!
+ */
+
+#include <stdio.h>
+
+int
+main()
+{
+	printf("Hello World! This src code is being written in OpenBSD KNF.");
+
+	return 0;
+} 
+/* WOW! Writing in this form is making me think.
+ * Even for such a simple program, I feel like im putting so much effort!
+ */
