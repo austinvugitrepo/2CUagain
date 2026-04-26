676bbc5692d5770b4ba75abb4f43deaf89392c20
Your Name <freeluck.v.i.p999@gmail.com>
2026-04-26 02:40:05 +0000

    more practice with general C concepts like last commit by updating my old head link list program i swear i will learn append nodes haha

--- headlinklist.c
+++ headlinklist.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <err.h>
 
 struct Node {
-	struct  Node *pos;
-	int	val;
-
+	struct Node *pos;
+	int val;
 };
 
 
-struct Node *hinsert(struct Node *, int);
+void hinsert(struct Node **, int);
 
 /* Program written to practice inserting at head of linked list. */
 
@@ -17,11 +17,11 @@ main(void)
 {
 	struct Node *head = NULL;
 
-	head = hinsert(head, 3);
+	hinsert(&head, 3);
 	printf("%d\n", head->val);
 		
-	head = hinsert(head, 2);
-	head = hinsert(head, 1);
+	hinsert(&head, 2);
+	hinsert(&head, 1);
 	
 	struct Node *cur = head;
 
@@ -46,13 +46,13 @@ main(void)
 
 /* This function helps faciliates inserting at head. */
 
-struct Node *
-hinsert(struct Node *ptr, int num)
+void
+hinsert(struct Node **ptr, int num)
 {
-	struct Node *h = malloc(sizeof(struct Node));
+	struct Node *h;
+	if((h = malloc(sizeof(struct Node))) == NULL)
+		err(1, NULL);
 	h->val = num;
-	h->pos = ptr;
-
-	return h;
-
+	h->pos = *ptr;
+	*ptr = h;
 }
